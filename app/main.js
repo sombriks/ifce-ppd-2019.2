3a1414cb4036faf6a564d18a419c370c259a3669
Leonardo Silveira <sombriks@gmail.com>
2019-08-17 15:19:11 -0300

    legal

--- app/main.js
+++ app/main.js
@@ -1,15 +1,17 @@
 import { app, BrowserWindow } from "electron";
 
-exports.startMeUp = _ => {
+export const startMeUp = _ => {
   app.on("ready", _ => {
     console.log("if you start me up i'll never stop, never stop");
     const win = new BrowserWindow({
       width: 800,
       height: 600,
+    //   frame: false,
       webPreferences: {
         nodeIntegration: true 
       }
     });
+    win.setMenuBarVisibility(false);
     win.loadFile("app/main.html"); 
   });
 };
