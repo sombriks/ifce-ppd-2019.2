0c01370ba107a08bba54f3de0c3e1b0113753028
Leonardo Silveira <sombriks@gmail.com>
2019-08-20 23:22:34 -0300

    skel

--- app/main.js
+++ app/main.js
@@ -1,9 +1,18 @@
 import { app, BrowserWindow, ipcMain } from "electron";
+import daemon from "./daemon";
+import host from "./host";
+import client from "./client";
+
+let win;
+
+ipcMain.on("join-host", (event, args) => {
+  console.log(args)
+  win.loadFile("app/html/lobby-chat.html");
+});
 
 export const startMeUp = _ => {
   app.on("ready", _ => {
-
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
       width: 800,
       height: 600,
       //   frame: false,
@@ -12,15 +21,6 @@ export const startMeUp = _ => {
       }
     });
     win.setMenuBarVisibility(false);
-
-    ipcMain.on("foo", (event, args) => {
-      console.log(args);
-      event.sender.send("bar", "pong!");
-    });
-
-    ipcMain.on("join-host", (event, args) => {
-      win.loadFile("app/html/lobby-chat.html");
-    });
     // initial state
     console.log("if you start me up i'll never stop, never stop");
     win.loadFile("app/html/open-screen.html");
