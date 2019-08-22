9fe74df0dc0ef1083cfbf519bd4318f9bf1c8e42
sombriks <sombriks@gmail.com>
2019-08-22 14:06:44 -0300

    trabalhos no esquema de desafio

--- app/client.js
+++ app/client.js
@@ -1,16 +1,21 @@
 export const myself = {};
 export const players = [];
 export const messages = [];
+export const challenges = [];
 export const games = [];
 
 export const join = async ({ ip, nickname }) => {
-  myself.nickname = nickname;
-  myself.id = nickname + "#" + new Date().getTime();
-  myself.status = "idle";
-  players.push(myself);
-  return "done!";
+	myself.nickname = nickname;
+	myself.id = nickname + "#" + new Date().getTime();
+	myself.status = "idle";
+	players.push(myself);
+	return "done!";
 };
 
 export const newMessage = async msg => {
-  return await messages.push(msg);
+	return await messages.push(msg);
+};
+
+export const newChallenge = async challenge => {
+	return await challenges.push(challenge);
 };
--- app/daemon.js
+++ app/daemon.js
@@ -2,33 +2,47 @@ import { ipcMain } from "electron";
 import * as host from "./host";
 import * as client from "./client";
 
-export const init = async (ipcMain, win) => {
-  ipcMain.on("join-host", (_, args) => {
-    const { ip, mode, nickname } = args;
-    if (mode == "host")
-      host.start().then(_ => {
-        client.join({ ip, nickname });
-        win.loadFile("app/html/lobby-chat.html");
-      });
-  });
+let win;
+
+export const init = async (ipcMain, _win) => {
+	win = _win;
+	ipcMain.on("join-host", (_, args) => {
+		const { ip, mode, nickname } = args;
+		if (mode == "host")
+			host.start().then(_ => {
+				client.join({ ip, nickname });
+				win.loadFile("app/html/lobby-chat.html");
+			});
+	});
 };
 
 ipcMain.on("get-myself", e => {
-  e.sender.send("myself", client.myself);
+	e.sender.send("myself", client.myself);
 });
 
 ipcMain.on("get-list-players", e => {
-  e.sender.send("list-players", client.players);
+	e.sender.send("list-players", client.players);
+});
+
+ipcMain.on("get-list-challenges", e => {
+	e.sender.send("list-challenges", client.challenges);
 });
 
 ipcMain.on("get-list-chat-messages", e => {
-  e.sender.send("list-chat-messages", client.messages);
+	e.sender.send("list-chat-messages", client.messages);
 });
 
 ipcMain.on("new-message", (e, msg) => {
-  msg.id = msg.player.id + "#" + new Date().getTime();
-  msg.stamp = new Date().toJSON();
-  client
-    .newMessage(msg)
-    .then(_ => e.sender.send("list-chat-messages", client.messages));
+	msg.id = msg.player.id + "#" + new Date().getTime();
+	msg.stamp = new Date().toJSON();
+	client
+		.newMessage(msg)
+		.then(_ => e.sender.send("list-chat-messages", client.messages));
+});
+
+ipcMain.on("send-challenge", (e, challenge) => {
+	client.newChallenge(challenge).then(_ => {
+		e.sender.send("list-challenges", client.challenges);
+		// win.loadFile("app/html/lobby-chat.html"); // TODO accept challenge
+	});
 });
