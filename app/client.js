f104bd672313a64762032a0431856cc295930c91
sombriks <sombriks@gmail.com>
2019-08-23 14:21:51 -0300

    falta no caso fazer o servidor socket e o board do jogo. tá bom já.

--- app/client.js
+++ app/client.js
@@ -6,30 +6,44 @@ export const games = [];
 export const myGame = {};
 
 export const join = async ({ ip, nickname }) => {
-  myself.nickname = nickname;
-  myself.id = nickname + "#" + new Date().getTime();
-  myself.status = "idle";
-  players.push(myself);
-  return "done!";
+	myself.nickname = nickname;
+	myself.id = nickname + "#" + new Date().getTime();
+	myself.status = "idle";
+	players.push(myself);
+	return "done!";
 };
 
 export const newMessage = async msg => {
-  return await messages.push(msg);
+	return await messages.push(msg);
 };
 
 export const newChallenge = async challenge => {
-  return await challenges.push(challenge);
+	return await challenges.push(challenge);
 };
 
 export const gotChallenge = challenge => {
-  return challenge.p1.id == myself.id || challenge.p2.id == myself.id;
+	return challenge.p1.id == myself.id || challenge.p2.id == myself.id;
 };
 
-export const acceptChallenge = challenge => {
+export const acceptChallenge = async challenge => {
+	myGame.challenge = challenge;
+	myGame.id = `game#${new Date().getTime()}#${challenge.id}`;
+	myGame.status = "open";
+	myGame.moves = [];
+	myGame.turn = 0;
+	games.push(myGame);
+	return games;
 };
 
 export const declineChallenge = challenge => {
-  let cha = challenges.filter(e => e.id != challenge.id);
-  challenges.length = 0;
-  challenges.push(...cha);
+	let cha = challenges.filter(e => e.id != challenge.id);
+	// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/length#Descri%C3%A7%C3%A3o
+	challenges.length = 0;
+	challenges.push(...cha);
+};
+
+export const gameMove = async game => {
+	myGame.moves = game.moves;
+	myGame.turn++;
+	return myGame;
 };
--- app/daemon.js
+++ app/daemon.js
@@ -5,56 +5,66 @@ import * as client from "./client";
 let win;
 
 export const init = async (ipcMain, _win) => {
-  win = _win;
-  ipcMain.on("join-host", (_, args) => {
-    const { ip, mode, nickname } = args;
-    if (mode == "host")
-      host.start().then(_ => {
-        client.join({ ip, nickname });
-        win.loadFile("app/html/lobby-chat.html");
-      });
-  });
+	win = _win;
+	ipcMain.on("join-host", (_, args) => {
+		const { ip, mode, nickname } = args;
+		if (mode == "host")
+			host.start().then(_ => {
+				client.join({ ip, nickname });
+				win.loadFile("app/html/lobby-chat.html");
+			});
+	});
 };
 
 ipcMain.on("get-myself", e => {
-  e.sender.send("myself", client.myself);
+	e.sender.send("myself", client.myself);
 });
 
 ipcMain.on("get-list-players", e => {
-  e.sender.send("list-players", client.players);
+	e.sender.send("list-players", client.players);
 });
 
 ipcMain.on("get-list-challenges", e => {
-  e.sender.send("list-challenges", client.challenges);
+	e.sender.send("list-challenges", client.challenges);
 });
 
 ipcMain.on("get-list-chat-messages", e => {
-  e.sender.send("list-chat-messages", client.messages);
+	e.sender.send("list-chat-messages", client.messages);
 });
 
 ipcMain.on("new-message", (e, msg) => {
-  msg.id = msg.player.id + "#" + new Date().getTime();
-  msg.stamp = new Date().toJSON();
-  client
-    .newMessage(msg)
-    .then(_ => e.sender.send("list-chat-messages", client.messages));
+	msg.id = msg.player.id + "#" + new Date().getTime();
+	msg.stamp = new Date().toJSON();
+	client
+		.newMessage(msg)
+		.then(_ => e.sender.send("list-chat-messages", client.messages));
 });
 
 ipcMain.on("send-challenge", (e, challenge) => {
-  client.newChallenge(challenge).then(_ => {
-    // todo mundo deve receber o challenge
-    e.sender.send("list-challenges", client.challenges);
-    // mas se o desafio envolver este player, botar o desafio na mesa
-    if (client.gotChallenge(challenge)) win.send("new-challenge", challenge);
-  });
+	client.newChallenge(challenge).then(_ => {
+		// todo mundo deve receber o challenge
+		e.sender.send("list-challenges", client.challenges);
+		// mas se o desafio envolver este player, botar o desafio na mesa
+		if (client.gotChallenge(challenge))
+			win.send("new-challenge", challenge);
+	});
 });
 
 ipcMain.on("accept-challenge", (e, challenge) => {
-  client.acceptChallenge(challenge);
-  win.loadFile("app/html/game-match.html");
+	client.acceptChallenge(challenge).then(_ => {
+		win.loadFile("app/html/game-match.html");
+	});
 });
 
 ipcMain.on("decline-challenge", (e, challenge) => {
-  client.declineChallenge(challenge);
-  e.sender.send("list-challenges", client.challenges);
+	client.declineChallenge(challenge);
+	e.sender.send("list-challenges", client.challenges);
+});
+
+ipcMain.on("get-my-game", e => {
+	e.sender.send("my-game", client.myGame);
+});
+
+ipcMain.on("send-game-move", (e, game) => {
+	client.gameMove(game).then(_ => e.sender.send(client.myGame));
 });
